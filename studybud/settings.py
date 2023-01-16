1175cb5943f6b6480c499d43a4b4d1d7524481d7
oseghale Ehichioya Israel <mrbrucekessy@gmail.com>
2023-01-16 14:31:05 +0100

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = (BASE_DIR, 'static')
+STATICFILES_DIRS = [(BASE_DIR / 'static')]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
